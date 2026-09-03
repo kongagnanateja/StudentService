92bded074280cc12cbd4c1fa8ba7cf99a5d00682
konga gnana teja <gnanateja392005@gmail.com>
2026-09-03 16:37:32 +0530

    hello

--- src/main/java/com/kongagnanateja/StudentService/controller/TestController.java
+++ src/main/java/com/kongagnanateja/StudentService/controller/TestController.java
@@ -7,12 +7,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/test")
 @CrossOrigin("*")
 public class TestController {
+
     @GetMapping("/hello")
-    public ResponseEntity<?> hello(){
+    public ResponseEntity<String> hello() {
         return new ResponseEntity("hello", HttpStatus.OK);
+    }
+
+    @GetMapping("/hii")
+    public ResponseEntity<?> hii() {
+        return new ResponseEntity("hii", HttpStatus.OK);
+
+    }
+    @GetMapping("/students")
+    public ResponseEntity<?> getStudents(){
+        Map<String, Integer> students =new HashMap<>();
+        students.put("hari",22);
+        students.put("Girl",32);
+        students.put("Siri",24);
+        students.put("Pari",22);
+        students.put("Jari",25);
+        return new ResponseEntity("students",HttpStatus.OK);
     }
 }
